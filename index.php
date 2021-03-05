3a2c3e94bc740ecf709fc5b6100206ea5deabee4
Sylvie-B <unsolo@hotmail.fr>
2021-03-05 15:21:50 +0100

    display com

--- index.php
+++ index.php
@@ -23,19 +23,19 @@ $db = $pdo->connect();
 
 // TODO Votre code ici.
 
-$request = $db->prepare("
-    SELECT article.id, article.title, article.content, categorie.name
-    FROM article
-    INNER JOIN categorie ON article.author_fk = categorie.id
-");
+//$request = $db->prepare("
+//    SELECT article.id, article.title, article.content, categorie.name
+//    FROM article
+//    INNER JOIN categorie ON article.author_fk = categorie.id
+//");
 
-$request->execute();
+//$request->execute();
 
-foreach ($request as $item){
-    echo "<div>Article ".$item['id']." : ".$item['title']." : ".$item['content']." Catégorie : ".$item['name']."</div>";
-}
-
-echo "<br>";
+//foreach ($request as $item){
+//    echo "<div>Article ".$item['id']." : ".$item['title']." : ".$item['content']." Catégorie : ".$item['name']."</div>";
+//}
+//
+//echo "<br>";
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
@@ -63,21 +63,39 @@ foreach ($request as $item){
 // TODO Votre code ici.
 
 try {
-    $sqlUt = "
-        INSERT INTO utilisateur (lastName, firstName, mail, password)
-        VALUES ('Doe', 'John', 'unknown@)nowhere.fr', 'motDePasse')
-    ";
+//    $sqlUt = "
+//        INSERT INTO utilisateur (lastName, firstName, mail, password)
+//        VALUES ('Doe', 'John', 'unknown@)nowhere.fr', 'motDePasse')
+//    ";
+//
+//    $addUt = $db->exec($sqlUt);
 
-    $addUt = $db->exec($sqlUt);
+//    $sqlCom = "
+//        INSERT INTO commentaire (content, user_fk, article_fk)
+//        VALUES ('Les jointures c\'est pas si dur', 2, 1),
+//               ('le SQL, il est pas cool !', 3, 2),
+//               ('C\'est loin le CSS', 1, 3)
+//    ";
 
-    $sqlCo = "
-        INSERT INTO commentaire (content, user_fk, article_fk)
-        VALUES ('Mon super commentaire', 3, 2)
-    ";
-
-    $addCo = $db->exec($sqlCo);
+//    $addCom = $db->exec($sqlCom);
 
 }
 catch (PDOException $exception){
     echo "add user failed : ".$exception->getMessage();
 }
+
+echo "<br>";
+
+$request2 = $db->prepare("
+    SELECT commentaire.id,commentaire.content, utilisateur.lastName, utilisateur.firstName
+    FROM commentaire
+        LEFT JOIN utilisateur ON commentaire.user_fk = utilisateur.id
+    ORDER BY commentaire.id
+");
+
+$request2->execute();
+
+foreach ($request2 as $com){
+    echo "<div>".$com['id']." ".$com['content']." ".$com['firstName']." ".$com['lastName']."</div>";
+}
+
